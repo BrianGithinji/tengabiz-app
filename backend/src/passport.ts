4a0b8053e25d2857f8a2068405e9072f928b57d0
BrianGithinji <briangithinji0001@gmail.com>
2026-09-24 01:38:09 +0300

    newer

--- backend/src/passport.ts
+++ backend/src/passport.ts
@@ -1,7 +1,7 @@
 import passport from 'passport'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
 import { randomUUID } from 'crypto'
-import pool from '../db.js'
+import pool from './db.js'
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID!,
